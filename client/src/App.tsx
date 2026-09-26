479bd27284f28fe96005aaad4ac33fd7ba477899
SuessVilliano <100315037+SuessVilliano@users.noreply.github.com>
2026-09-26 14:08:59 -0400

    Suppress loading chrome on Trade Hybrid TV

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -172,7 +172,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <CyberpunkLoadingScreen isLoading={isInitialLoading && !/^\/tradehouse\/(stage|broadcast)(\/|$)/.test(window.location.pathname)} />
+        <CyberpunkLoadingScreen isLoading={isInitialLoading && !/^\/tradehouse\/(stage|broadcast|tv)(\/|$)/.test(window.location.pathname)} />
         <Router>
           <RouteScrollManager />
           <AppRouter />
